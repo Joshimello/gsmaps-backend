fix: remove everything except rotation

source/extensions/my_company.my_usd_viewer_messaging_extension/my_company/my_usd_viewer_messaging_extension/camera_control.py
@@ -43,28 +43,29 @@ class CameraManager:
             position = payload['position']  # e.g., [0.0, 0.0, 10.0]
             euler_angles = payload['rotation']  # e.g., [0.0, 0.0, 0.0]
 
-            euler_angles = np.radians(euler_angles)
+            print(euler_angles)
+            eular_degrees = np.degrees(euler_angles)
 
-            C2W = np.eye(4)
-            C2W[:3, :3] = Rotation.from_euler('xyz', euler_angles).as_matrix()
-            C2W[:3, 3] = position
+            # C2W = np.eye(4)
+            # C2W[:3, :3] = Rotation.from_euler('xyz', euler_angles).as_matrix()
+            # C2W[:3, 3] = position
 
-            W2C = np.linalg.inv(C2W)
+            # W2C = np.linalg.inv(C2W)
 
-            ISAAC_SIM_TO_GS_CONVENTION = np.array([
-                [1,  0,  0, 0],
-                [0, -1,  0, 0],
-                [0,  0, -1, 0],
-                [0,  0,  0, 1]
-            ])
-            W2C = ISAAC_SIM_TO_GS_CONVENTION @ W2C
+            # ISAAC_SIM_TO_GS_CONVENTION = np.array([
+            #     [1,  0,  0, 0],
+            #     [0, -1,  0, 0],
+            #     [0,  0, -1, 0],
+            #     [0,  0,  0, 1]
+            # ])
+            # W2C = ISAAC_SIM_TO_GS_CONVENTION @ W2C
 
-            R = W2C[:3, :3].T
-            T = W2C[:3, 3]
+            # R = W2C[:3, :3].T
+            # T = W2C[:3, 3]
 
-            rotation = Rotation.from_matrix(R).as_euler('yxz', degrees=True)
+            # rotation = Rotation.from_matrix(R).as_euler('yxz', degrees=True)
 
-            rotation = [math.degrees(angle) for angle in rotation]
+            # rotation = [math.degrees(angle) for angle in rotation]
 
             ctx = omni.usd.get_context()
             stage = ctx.get_stage()
@@ -73,14 +74,14 @@ class CameraManager:
 
             if camera_prim:
                 with Usd.EditContext(stage, Usd.EditTarget(stage.GetSessionLayer())):
-                    translate_attr = camera_prim.GetAttribute('xformOp:translate')
-                    if translate_attr:
-                        translate_attr.Set(tuple(position))
+            #         translate_attr = camera_prim.GetAttribute('xformOp:translate')
+            #         if translate_attr:
+            #             translate_attr.Set(tuple(position))
 
                     rotate_attr = camera_prim.GetAttribute('xformOp:rotateYXZ')
                     if rotate_attr:
-                        rotate_attr.Set(tuple([0, -rotation[1], 0]))  # [y_rot, x_rot, z_rot]
-                print(f"Teleported camera to position {position} and looking at target with rotation YXZ {rotation}")
+                        rotate_attr.Set(tuple([-eular_degrees[1], eular_degrees[0], 0]))  # [y_rot, x_rot, z_rot]
+            #     print(f"Teleported camera to position {position} and looking at target with rotation YXZ {rotation}")
 
 
 